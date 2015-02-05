d58e1fe0f2e66c9fb2d64fb2da20ef2e7ee6c5e4
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-02-05 11:47:49 +0530

    Initial Commit

---
+++ BackgroundApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroundApp
+//
+//  Created by Vinod Vishwakarma on 05/02/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackgroundApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundApp
+//
+//  Created by Vinod Vishwakarma on 05/02/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
